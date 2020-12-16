867a24ffe41c204a4537d8aeb0f0134061c78186
manikandanraji <manikandan48711113@gmail.com>
2020-12-16 17:42:41 +0530

    added branchsums solution and question

---
+++ day1/branchsums/branchsums.cpp
@@ -0,0 +1,28 @@
+#include <cstddef>
+#include <vector>
+
+struct Node {
+	int data;
+	Node *left;
+	Node *right;
+}
+
+void calculate_branch_sums(Node *tree, int runningSum, vector<int> &sums) {
+	if(tree == NULL) return;
+
+	runningSum += tree->data;
+	if(tree->left == NULL && tree->right == NULL) {
+		sums.push_back(runningSum)
+		return;
+	}
+
+	calculate_branch_sums(tree->left, runningSum, sums);
+	calculate_branch_sums(tree->right, runningSum, sums);
+}
+
+// O(n) time / O(n) space <--- start here
+vector<int> branch_sums(Node *root) {
+	vector<int> sums {};
+	calculate_branch_sums(root, 0, sums)
+	return sums;
+}
