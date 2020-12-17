e543b213cedf108b42ef88966889aeb81a59789f
manikandanraji <manikandan48711113@gmail.com>
2020-12-17 21:15:31 +0530

    formatted header files

--- day1/nearest_element_in_bst/src/bst.h
+++ day1/nearest_element_in_bst/src/bst.h
@@ -2,16 +2,16 @@
 #define BST_H
 
 class BST {
-	public:
-		int value;
-		BST *left;
-		BST *right;
+ public:
+  int value;
+  BST *left;
+  BST *right;
 
-	public:
-		BST(int value);
-		void insert(int value);
-		bool contains(int value);
-		int get_min_val();
+ public:
+  BST(int value);
+  void insert(int value);
+  bool contains(int value);
+  int get_min_val();
 };
 
 #endif
--- day2/bst_construction/src/bst.h
+++ day2/bst_construction/src/bst.h
@@ -2,15 +2,15 @@
 #define BST_H
 
 class BST {
-	int value;
-	BST *left;
-	BST *right;
+  int value;
+  BST *left;
+  BST *right;
 
-	public:
-		BST(int value);
-		void insert(int value);
-		bool contains(int value);
-		int get_min_val();
+ public:
+  BST(int value);
+  void insert(int value);
+  bool contains(int value);
+  int get_min_val();
 };
 
 #endif
