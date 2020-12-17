c087f040267b691061ae50c30fb14804bee0d17b
manikandanraji <manikandan48711113@gmail.com>
2020-12-17 08:53:53 +0530

    added nth_fib with unit tesing

---
+++ day2/nth_fib/src/main.cpp
@@ -0,0 +1,25 @@
+#include "gtest/gtest.h"
+#include "nth_fib.h"
+
+TEST(NthFibTest, TestsNaiveSolution) {
+	EXPECT_EQ(nth_fib_memoize(1), 0);
+	EXPECT_EQ(nth_fib_memoize(2), 1);
+	EXPECT_EQ(nth_fib_memoize(6), 5);
+}
+
+TEST(NthFibTest, TestsMemoizeSolution) {
+	EXPECT_EQ(nth_fib_memoize(1), 0);
+	EXPECT_EQ(nth_fib_memoize(2), 1);
+	EXPECT_EQ(nth_fib_memoize(6), 5);
+}
+
+TEST(NthFibTest, TestsIterativeSolution) {
+	EXPECT_EQ(nth_fib_iterative(1), 0);
+	EXPECT_EQ(nth_fib_iterative(2), 1);
+	EXPECT_EQ(nth_fib_iterative(6), 5);
+}
+
+int main(int argc, char **argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
---
+++ day2/nth_fib/src/nth_fib.h
@@ -0,0 +1,8 @@
+#ifndef NTH_FIB_H
+#define NTH_FIB_H
+
+int nth_fib_naive(int n);
+int nth_fib_memoize(int n);
+int nth_fib_iterative(int n);
+
+#endif
